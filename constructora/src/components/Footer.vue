ceb5c241b64246fd9ec036f9c6a3946f978bb134
erick <erickitofern@gmail.com>
2020-01-17 07:47:53 -0800

    se realizo footer

---
+++ constructora/src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card text-center">
+  <div class="card-header">
+    
+  </div>
+  <div class="container">
+  <div class="row">
+    <div class="col-sm">
+       <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+  </div>
+    </div>
+    <div class="col-sm">
+       <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+  </div>
+    </div>
+    <div class="col-sm">
+       <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+  </div>
+    </div>
+  </div>
+</div>
+  <div class="bg-danger">
+    akssjfhalskffhvs;orfhuwrovh
+  </div>
+</div>
+</template>
