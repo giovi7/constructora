2ce78e2fcceea5713f930915bc5b34db4bde1870
giovi7 <giovi7azul@gmail.com>
2020-01-28 10:20:10 -0400

    avance de vistas

--- constructora/src/components/Carousel.vue
+++ constructora/src/components/Carousel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="mt-0">
     <b-carousel
       id="carousel-1"
       v-model="slide"
@@ -17,38 +17,17 @@
       <b-carousel-slide
         caption="First slide"
         text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=52"
+        :img-src="require('../assets/Constructora/foto26.jpg')"
       ></b-carousel-slide>
 
       <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+      <b-carousel-slide :img-src="require('../assets/Constructora/foto24.jpg')">
         <h1>Hello world!</h1>
       </b-carousel-slide>
 
       <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template v-slot:img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="https://picsum.photos/1024/480/?image=55"
-            alt="image slot"
-          >
-        </template>
-      </b-carousel-slide>
-
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-        </p>
-      </b-carousel-slide>
+      <b-carousel-slide :img-src="require('../assets/Constructora/foto7.jpg')"></b-carousel-slide>
+    
     </b-carousel>
 
     <p class="mt-4">
--- constructora/src/components/Servi.vue
+++ constructora/src/components/Servi.vue
@@ -5,16 +5,15 @@
       <br />
     </ul>
     <div>
-  <div>
-    <h4></h4>
+  <div class="mt-4">
     <b-card-group deck>
-      <b-card img-src="https://placekitten.com/1000/300" img-alt="Card image" img-top>
+      <b-card  :img-src="require('../assets/Constructora/foto4.jpg')" img-alt="Card image" img-top>
         <b-card-text>
           <h5>OBRA GRUESA.</h5> 
         </b-card-text>
       </b-card>
 
-      <b-card img-src="https://placekitten.com/1000/300" img-alt="Card image" img-bottom>
+      <b-card :img-src="require('../assets/Constructora/foto46.jpg')" img-alt="Card image" img-bottom>
         <b-card-text>
           <h5>OBRA FINA.</h5>
         </b-card-text>
@@ -23,14 +22,13 @@
   </div>
   <div class="mt-4">
     <h4></h4>
-    <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-left class="mb-3">
+    <b-card :img-src="require('../assets/Constructora/casa.jpg')" img-alt="Card image" img-left class="mb-3">
       <b-card-text>
-      
         <h5>LLAVE A LA MANO</h5>
       </b-card-text>
     </b-card>
 
-    <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-right>
+    <b-card :img-src="require('../assets/Constructora/casas.jpg')" img-alt="Card image" img-right>
       <b-card-text>
        <h5>REMODELACION</h5>
       </b-card-text>
