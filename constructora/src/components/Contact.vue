dc842cd2402954932dc97643f6cf0ecf7903d726
erick <erickitofern@gmail.com>
2020-02-04 14:28:25 -0800

    se trabajo

--- constructora/src/components/Contact.vue
+++ constructora/src/components/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    style="background-image: url('') "
+    class="fondo" 
   >
     <b-row>
       <b-col>
@@ -96,9 +96,7 @@
         </b-col>
       </b-col>
 
-   
-    </b-row>
-    <b-col>
+   <b-col>
       <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d470397.7683350531!2d-68.47881615491423!3d-22.91401391012919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x96a84c7aa92abf3f%3A0x7af4614cacf997d1!2sSan+Pedro+de+Atacama%2C+Antofagasta+Region!5e0!3m2!1sen!2scl!4v1473271159931"
     
@@ -107,6 +105,8 @@
       allowfullscreen
     ></iframe>
       </b-col>
+    
+    </b-row>
     
   </div>
 </template>
@@ -118,13 +118,7 @@ export default {};
 </script>
     
  <style>
-@import url("https://fonts.googleapis.com/css?family=Varela+Round");
 
-#app {
-  font-family: "Varela Round", Helvetica, Arial, sans-serif;
-
-  color: #2c3e50;
-}
 
 .google-maps iframe {
 position: absolute;
@@ -132,6 +126,13 @@ top: 0;
 left: 0;
 width: 100% !important;
 height: 100% !important;
+}
+.fondo{
+  background: linear-gradient(217deg, rgb(245, 246, 243), rgba(246, 242, 241, 0.452) 70.71%),
+            linear-gradient(127deg, rgb(227, 229, 231), rgba(211, 221, 221, 0.897) 70.71%),
+            linear-gradient(336deg, rgb(247, 250, 250), rgba(194, 240, 216, 0.52) 70.71%);
+            
+ /* background: linear-gradient(to bottom, rgb(255, 254, 254), rgb(131, 130, 130)); */
 }
 </style>
 
