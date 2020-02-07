f3130506341b71750e86e7cba118ecf918e1b28c
erick <erickitofern@gmail.com>
2020-02-07 11:40:33 -0800

    conclucion del proyecto

--- constructora/src/components/Footer.vue
+++ constructora/src/components/Footer.vue
@@ -7,16 +7,17 @@
             <div class>
               <br />
               <br />
-              <br />
-              <img src="@/assets/logo02.jpg" width="200" >
+
+              <img src="@/assets/Constructora/logoss.png" width="170" />
             </div>
           </div>
         </b-col>
         <b-col>
           <div class="col-sm">
-            <br />
             <div class="card-body">
               <strong class="text-dark">Nuestro Objetivo</strong>
+              <br />
+              <br />
               <p
                 class="text-dark"
               >satisfacción con nuestros clientes al adquirir exclusividad y calidad con nuestros trabajos realizados.</p>
@@ -24,75 +25,73 @@
           </div>
         </b-col>
         <b-col>
-          <br>
+          <br />
           <strong class="text-dark">Contactenos</strong>
-          <div class="col-sm"> 
-            <div class="card-deck">
-            
-              <div >
+          <div class="col-sm">
+            <div class>
+              <div>
+                <img
+                  class="alignnone size-full wp-image-351"
+                  src="https://lh5.googleusercontent.com/proxy/yqH7YvOrZJra9iv5nDpxdg66xnhz-mJqVjTByjQNUDIyjg4FcGOQ2vLypXWzJdFNefDvgq1OW_nSoIgQj7XOh8Q5emKFx4j8BkfbY7mqWs_A"
+                  width="35"
+                  height
+                />
+                <br />
+
                 <a
                   class="text-dark"
-                  href="mailto:giovi7azul@gmail.com?Subject=Interesado%20en%20el%20curso"
-                >
-                  <img
-                   
-                    src="https://lh5.googleusercontent.com/proxy/yqH7YvOrZJra9iv5nDpxdg66xnhz-mJqVjTByjQNUDIyjg4FcGOQ2vLypXWzJdFNefDvgq1OW_nSoIgQj7XOh8Q5emKFx4j8BkfbY7mqWs_A"
-                    width="39"
-                    height
-                      
-                  />
-                </a>
+                  href="mailto:josefinobranez@gmail.com?Subject=Interesado%20en%20el%20curso"
+                >Contactar por correo</a>
+                <br />
               </div>
-              <div class="">
-
-               <div id="comp-jim2j5xfinlineContent-gridWrapper" data-mesh-internal="true">
-                <div id="comp-jim2j5xfinlineContent-gridContainer" data-mesh-internal="true">
-                  <div
-                    title
-                    data-is-responsive="false"
-                    data-display-mode="fill"
-                    data-content-padding-horizontal="0"
-                    data-content-padding-vertical="0"
-                    data-exact-height="71"
-                    class="ca1"
-                    id="comp-jim2rqpq"
-                  >
-                    <a
-                      href="https://api.whatsapp.com/send?phone=59167449227"
-                      target="_blank"
-                      data-content="https://api.whatsapp.com/send?phone=59167449227"
-                      data-type="external"
-                      rel="noopener noreferrer"
-                      id="comp-jim2rqpqlink"
-                      class="ca1link"
-                      style="cursor: pointer; width: 72px; height: 71px;"
+              <div class="mt-1">
+                <div id="comp-jim2j5xfinlineContent-gridWrapper" data-mesh-internal="true">
+                  <div id="comp-jim2j5xfinlineContent-gridContainer" data-mesh-internal="true">
+                    <div
+                      title
+                      data-is-responsive="false"
+                      data-display-mode="fill"
+                      data-content-padding-horizontal="0"
+                      data-content-padding-vertical="0"
+                      data-exact-height="71"
+                      class="ca1"
+                      id="comp-jim2rqpq"
                     >
-                      <wix-image
-                        data-has-bg-scroll-effect
-                        data-image-info="{&quot;imageData&quot;:{&quot;type&quot;:&quot;Image&quot;,&quot;id&quot;:&quot;dataItem-jim2rqrf&quot;,&quot;metaData&quot;:{&quot;pageId&quot;:&quot;cv7wy&quot;,&quot;isPreset&quot;:false,&quot;schemaVersion&quot;:&quot;1.0&quot;,&quot;isHidden&quot;:false},&quot;title&quot;:&quot;&quot;,&quot;uri&quot;:&quot;d7551e_3fff401c806d4abeaabfb0d192c0b1ab~mv2.png&quot;,&quot;description&quot;:&quot;&quot;,&quot;width&quot;:512,&quot;height&quot;:512,&quot;alt&quot;:&quot;&quot;,&quot;link&quot;:{&quot;type&quot;:&quot;ExternalLink&quot;,&quot;id&quot;:&quot;dataItem-jim5qfmq&quot;,&quot;metaData&quot;:{&quot;pageId&quot;:&quot;cv7wy&quot;,&quot;isPreset&quot;:false,&quot;schemaVersion&quot;:&quot;1.0&quot;,&quot;isHidden&quot;:false},&quot;url&quot;:&quot;https://api.whatsapp.com/send?phone=59167449227&quot;,&quot;target&quot;:&quot;_blank&quot;},&quot;quality&quot;:{&quot;quality&quot;:90,&quot;unsharpMask&quot;:{&quot;radius&quot;:0,&quot;amount&quot;:0,&quot;threshold&quot;:0}},&quot;displayMode&quot;:&quot;fill&quot;},&quot;containerId&quot;:&quot;comp-jim2rqpq&quot;,&quot;displayMode&quot;:&quot;fill&quot;}"
-                        data-has-ssr-src
-                        data-is-svg="false"
-                        id="comp-jim2rqpqimg"
-                        class="ca1img"
-                        style="width: 72px; height: 71px; object-fit: cover;"
-                        data-src="https://static.wixstatic.com/media/d7551e_3fff401c806d4abeaabfb0d192c0b1ab~mv2.png/v1/fill/w_72,h_71,al_c,q_95/d7551e_3fff401c806d4abeaabfb0d192c0b1ab~mv2.webp"
+                      <a
+                        href="https://api.whatsapp.com/send?phone=56 9 9891 9642"
+                        target="_blank"
+                        data-content="https://api.whatsapp.com/send?phone=56 9 9891 9642"
+                        data-type="external"
+                        rel="noopener noreferrer"
+                        id="comp-jim2rqpqlink"
+                        class="ca1link"
                       >
-                        <img
-                          class="text-right"
-                          id="comp-jim2rqpqimgimage"
-                          alt
-                          data-type="image"
-                          itemprop="image"
-                          src="https://www.stickpng.com/assets/images/5ae21cc526c97415d3213554.png"
+                        <wix-image
+                          data-has-bg-scroll-effect
+                          data-image-info="{&quot;imageData&quot;:{&quot;type&quot;:&quot;Image&quot;,&quot;id&quot;:&quot;dataItem-jim2rqrf&quot;,&quot;metaData&quot;:{&quot;pageId&quot;:&quot;cv7wy&quot;,&quot;isPreset&quot;:false,&quot;schemaVersion&quot;:&quot;1.0&quot;,&quot;isHidden&quot;:false},&quot;title&quot;:&quot;&quot;,&quot;uri&quot;:&quot;d7551e_3fff401c806d4abeaabfb0d192c0b1ab~mv2.png&quot;,&quot;description&quot;:&quot;&quot;,&quot;width&quot;:512,&quot;height&quot;:512,&quot;alt&quot;:&quot;&quot;,&quot;link&quot;:{&quot;type&quot;:&quot;ExternalLink&quot;,&quot;id&quot;:&quot;dataItem-jim5qfmq&quot;,&quot;metaData&quot;:{&quot;pageId&quot;:&quot;cv7wy&quot;,&quot;isPreset&quot;:false,&quot;schemaVersion&quot;:&quot;1.0&quot;,&quot;isHidden&quot;:false},&quot;url&quot;:&quot;https://api.whatsapp.com/send?phone=56 9 9891 9642&quot;,&quot;target&quot;:&quot;_blank&quot;},&quot;quality&quot;:{&quot;quality&quot;:90,&quot;unsharpMask&quot;:{&quot;radius&quot;:0,&quot;amount&quot;:0,&quot;threshold&quot;:0}},&quot;displayMode&quot;:&quot;fill&quot;},&quot;containerId&quot;:&quot;comp-jim2rqpq&quot;,&quot;displayMode&quot;:&quot;fill&quot;}"
+                          data-has-ssr-src
+                          data-is-svg="false"
+                          id="comp-jim2rqpqimg"
+                          class="ca1img"
                           style="width: 72px; height: 71px; object-fit: cover;"
-                        />
-                      </wix-image>
-                    </a>
+                          data-src="https://static.wixstatic.com/media/d7551e_3fff401c806d4abeaabfb0d192c0b1ab~mv2.png/v1/fill/w_72,h_71,al_c,q_95/d7551e_3fff401c806d4abeaabfb0d192c0b1ab~mv2.webp"
+                        >
+                          <img
+                            class="text-right"
+                            id="comp-jim2rqpqimgimage"
+                            alt
+                            data-type="image"
+                            itemprop="image"
+                            src="https://www.stickpng.com/assets/images/5ae21cc526c97415d3213554.png"
+                            style="width: 50px;"
+                          />
+                          <p>whattssap</p>
+                        </wix-image>
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div> 
               </div>
-              
             </div>
           </div>
         </b-col>
