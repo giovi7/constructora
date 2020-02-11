486d19b3b3badd0cd6c222ef3a3f8aac321d1f0c
erick <erickitofern@gmail.com>
2020-02-11 06:53:13 -0800

    se sube algunos cambios

--- constructora/src/components/Construc.vue
+++ constructora/src/components/Construc.vue
@@ -1,40 +1,31 @@
 
 <template>
-  <div  class="fondo">
+  <div class="fondo">
     <div>
       <br />
 
-      <h1 class="text-dark">
-        
-      </h1>
+      <h1 class="text-dark"></h1>
       <br />
-
-      <b-container>
-        <div class="card bg-dark text-white">
-    
-           <img src="@/assets/Constructora/foto055.gif" class="card-img" alt="..." width="" />
-          <div class="card-img-overlay"> <br>
-            <h2 class="card-title"><strong class="text-dark">Constructora</strong></h2>
-            <p
-              class="text-dark"
-            > 
-            <br> <strong>
+      <h2>
+        <strong class="text-dark">Constructora</strong>
+      </h2>
+      <p class="text-dark">
+        <br />
+        <strong>
           La constructora se encuentra ubicada en San Pedro de Atacama CHILE está muy ligada a la arquitectura que el cliente pida. Sus antiguas casas de adobe, bloques, madera, son símbolo de la localidad en la zona en que se encuentran ya que la zona es un lugar turístico.
           En cuanto a construcciones nuevas, existe una norma para regular que restringe la altura y colores de las viviendas y propone fachadas continuas en centros patrimoniales, entre otros puntos de la zona.
           La constructora se dedica a la construcción de casas, cabañas y hostales y también realiza trabajos en: llave a la mano (es cuando la constructora se encarga de la obra gruesa, obra fina y el acabado final), gafiter (plomeria), electricidad, remodelación, obra gruesa, obra fina.
-          </strong> <br> <br> <br>
-       <div class="card-deck"> <br>
-          <div class="card">
-            <img src="@/assets/Constructora/foto033.gif" class="card-img-top" alt="..." width="100" />
-          </div>
-          <div class="card">
-            <img src="@/assets/Constructora/foto018.gif" class="card-img-top" alt="..." width="100" />
-          </div>
+        </strong> <br> <br>
+      </p>
+      <div class="card-deck">
+        <br />
+        <div class="card">
+          <img src="@/assets/Constructora/foto033.gif" class="card-img-top" alt="..." width="90" />
         </div>
-          </div>
+        <div class="card">
+          <img src="@/assets/Constructora/foto018.gif" class="card-img-top" alt="..." width="90" />
         </div>
-       
-      </b-container>
+      </div>
     </div>
   </div>
 </template>
@@ -43,18 +34,23 @@
 export default {};
 </script>
  <style>
-@import url("https://fonts.googleapis.com/css?family=Varela+Round");
+.fondo {
+  background: linear-gradient(
+      217deg,
+      rgb(245, 246, 243),
+      rgba(246, 242, 241, 0.452) 70.71%
+    ),
+    linear-gradient(
+      127deg,
+      rgb(227, 229, 231),
+      rgba(211, 221, 221, 0.897) 70.71%
+    ),
+    linear-gradient(
+      336deg,
+      rgb(247, 250, 250),
+      rgba(194, 240, 216, 0.52) 70.71%
+    );
 
-#app {
-  font-family: "Varela Round", Helvetica, Arial, sans-serif;
-
-  color: #2c3e50;
-}
-.fondo{
-  background: linear-gradient(217deg, rgb(245, 246, 243), rgba(246, 242, 241, 0.452) 70.71%),
-            linear-gradient(127deg, rgb(227, 229, 231), rgba(211, 221, 221, 0.897) 70.71%),
-            linear-gradient(336deg, rgb(247, 250, 250), rgba(194, 240, 216, 0.52) 70.71%);
-            
- /* background: linear-gradient(to bottom, rgb(255, 254, 254), rgb(131, 130, 130)); */
+  /* background: linear-gradient(to bottom, rgb(255, 254, 254), rgb(131, 130, 130)); */
 }
 </style>
--- constructora/src/components/Footer.vue
+++ constructora/src/components/Footer.vue
@@ -33,65 +33,24 @@
                 <img
                   class="alignnone size-full wp-image-351"
                   src="https://lh5.googleusercontent.com/proxy/yqH7YvOrZJra9iv5nDpxdg66xnhz-mJqVjTByjQNUDIyjg4FcGOQ2vLypXWzJdFNefDvgq1OW_nSoIgQj7XOh8Q5emKFx4j8BkfbY7mqWs_A"
-                  width="35"
+                  width="30"
                   height
                 />
                 <br />
 
                 <a
-                  class="text-dark"
+                  class="text-primary"
                   href="mailto:josefinobranez@gmail.com?Subject=Interesado%20en%20el%20trabajo"
-                >Contactar por correo</a>
+                >Correo electronico</a>
                 <br />
               </div>
-              <div class="mt-1">
-                <div id="comp-jim2j5xfinlineContent-gridWrapper" data-mesh-internal="true">
-                  <div id="comp-jim2j5xfinlineContent-gridContainer" data-mesh-internal="true">
-                    <div
-                      title
-                      data-is-responsive="false"
-                      data-display-mode="fill"
-                      data-content-padding-horizontal="0"
-                      data-content-padding-vertical="0"
-                      data-exact-height="71"
-                      class="ca1"
-                      id="comp-jim2rqpq"
-                    >
-                      <a
-                        href="https://api.whatsapp.com/send?phone=56 9 9891 9642"
-                        target="_blank"
-                        data-content="https://api.whatsapp.com/send?phone=56 9 9891 9642"
-                        data-type="external"
-                        rel="noopener noreferrer"
-                        id="comp-jim2rqpqlink"
-                        class="ca1link"
-                      >
-                        <wix-image
-                          data-has-bg-scroll-effect
-                          data-image-info="{&quot;imageData&quot;:{&quot;type&quot;:&quot;Image&quot;,&quot;id&quot;:&quot;dataItem-jim2rqrf&quot;,&quot;metaData&quot;:{&quot;pageId&quot;:&quot;cv7wy&quot;,&quot;isPreset&quot;:false,&quot;schemaVersion&quot;:&quot;1.0&quot;,&quot;isHidden&quot;:false},&quot;title&quot;:&quot;&quot;,&quot;uri&quot;:&quot;d7551e_3fff401c806d4abeaabfb0d192c0b1ab~mv2.png&quot;,&quot;description&quot;:&quot;&quot;,&quot;width&quot;:512,&quot;height&quot;:512,&quot;alt&quot;:&quot;&quot;,&quot;link&quot;:{&quot;type&quot;:&quot;ExternalLink&quot;,&quot;id&quot;:&quot;dataItem-jim5qfmq&quot;,&quot;metaData&quot;:{&quot;pageId&quot;:&quot;cv7wy&quot;,&quot;isPreset&quot;:false,&quot;schemaVersion&quot;:&quot;1.0&quot;,&quot;isHidden&quot;:false},&quot;url&quot;:&quot;https://api.whatsapp.com/send?phone=56 9 9891 9642&quot;,&quot;target&quot;:&quot;_blank&quot;},&quot;quality&quot;:{&quot;quality&quot;:90,&quot;unsharpMask&quot;:{&quot;radius&quot;:0,&quot;amount&quot;:0,&quot;threshold&quot;:0}},&quot;displayMode&quot;:&quot;fill&quot;},&quot;containerId&quot;:&quot;comp-jim2rqpq&quot;,&quot;displayMode&quot;:&quot;fill&quot;}"
-                          data-has-ssr-src
-                          data-is-svg="false"
-                          id="comp-jim2rqpqimg"
-                          class="ca1img"
-                          style="width: 72px; height: 71px; object-fit: cover;"
-                          data-src="https://static.wixstatic.com/media/d7551e_3fff401c806d4abeaabfb0d192c0b1ab~mv2.png/v1/fill/w_72,h_71,al_c,q_95/d7551e_3fff401c806d4abeaabfb0d192c0b1ab~mv2.webp"
-                        >
-                          <img
-                            class="text-right"
-                            id="comp-jim2rqpqimgimage"
-                            alt
-                            data-type="image"
-                            itemprop="image"
-                            src="https://www.stickpng.com/assets/images/5ae21cc526c97415d3213554.png"
-                            style="width: 50px;"
-                          />
-                          <p>whattssap</p>
-                        </wix-image>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <a target="_black" href="https://api.whatsapp.com/send?phone=56 9 9891 9642">
+                <img
+                  src="https://cdn.icon-icons.com/icons2/1488/PNG/128/5302-whatsapp_102550.png"
+                  width="35"
+                  height
+                /> Whatsapp
+              </a>
             </div>
           </div>
         </b-col>
